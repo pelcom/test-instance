57b0527701fa3bff8372e8567008bfd6de9e1255
pelcom <olaawopelumi@gmail.com>
2016-10-26 03:46:34 +0100

    added socket.io

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req,res){
+    
+    res.status(200).send('Hello from the oter side. ');
+    
+});
+
+var server = app.listen(process.env.PORT|| '8080', function(){
+    
+    console.log('App listening on port %s',server.address().port);
+    console.log('Press Ctrl + C to quit');
+});
